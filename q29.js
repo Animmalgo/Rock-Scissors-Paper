2a2c06205877e431f93b4753debe8129c53515d9
Animmalgo <132540346+Animmalgo@users.noreply.github.com>
2023-05-11 10:19:01 +0900

    Update q29.js
    v0.1.1

--- q29.js
+++ q29.js
@@ -1,4 +1,4 @@
-// 가위바위보 v0.1
+// 가위바위보 v0.1.1
 
 // 컴퓨터 가위바위보 랜덤 함수
 var com = Math.floor(Math.random() * 3 + 1);
@@ -10,24 +10,10 @@ var player = prompt("가위 바위 보 중 하나를 입력하세요");
 var scissors = "가위";
 var rock = "바위";
 var paper = "보";
+var winDrawLose = "";
 
-// 플레이어 입력 값 탐지
-if (player == scissors) {
-    var player = scissors;
-    document.write(player);
-} else if (player == rock) {
-    var player = rock;
-    document.write(player);
-} else if ( player == paper ) {
-    var player = paper;
-    document.write(player);
-} else {
-    document.write("가위, 바위, 보 중 하나를 입력해주세요");
-}
-
-document.write("<hr>") // 플레이어, 컴퓨터 임시 구분라인
-
-console.log(com) // 컴퓨터 랜덤 확인용
+// 컴퓨터 랜덤 확인용
+console.log(com) 
 
 // 컴퓨터 랜덤값에 가위바위보 적용
 if (com == 1) {
@@ -40,3 +26,74 @@ if (com == 1) {
     var com = paper;
     document.write(com);
 }
+
+// 컴퓨터, 플레이어 구분라인
+document.write("<hr>") 
+
+// 플레이어 입력 값 탐지
+// if (player == scissors) {
+//     var player = scissors;
+//     document.write(player);
+// } else if (player == rock) {
+//     var player = rock;
+//     document.write(player);
+// } else if ( player == paper ) {
+//     var player = paper;
+//     document.write(player);
+// } else {
+//     document.write("가위, 바위, 보 중 하나를 입력해주세요");
+// }
+
+// 승패여부
+switch(player) {
+    case "가위":
+        document.write(player);
+        switch(com) {
+            case "가위":
+                winDrawLose = "무승부";
+                break;
+            case "바위":
+                winDrawLose = "패배";
+                break;
+            case "보":
+                winDrawLose = "승리";
+                break;
+        }
+        break;
+
+    case "바위":
+        document.write(player);
+        switch(com) {
+            case "가위":
+                winDrawLose = "승리";
+                break;
+            case "바위":
+                winDrawLose = "무승부";
+                break;
+            case "보":
+                winDrawLose = "패배";
+                break;
+        }
+        break;
+
+    case "보":
+        document.write(player);
+        switch(com) {
+            case "가위":
+                winDrawLose = "패배";
+                break;
+            case "바위":
+                winDrawLose = "승리";
+                break;
+            case "보":
+                winDrawLose = "무승부";
+                break;
+        }
+        break;
+    default:
+        document.write("가위, 바위, 보 중 하나를 입력해주세요");
+        break;
+}
+
+// 승패여부 출력
+document.write("<hr>"+ winDrawLose )
